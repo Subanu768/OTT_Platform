8959f53b4eccd65465241104f02825345de10507
Subanu768 <subanusriv@gmail.com>
2025-04-17 12:41:15 +0530

    Update Login page and add validation utility

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,82 @@
 import React, { useState } from 'react'
 import './Login.css'
+import { validateSignInValues, validateSignUpValues } from "../utils/validate";
 import logo from '../../assets/logo.png'
 import { login, signup } from '../../firebase'
 import netflix_spinner from '../../assets/netflix_spinner.gif'
+import {useDispatch} from 'react-redux'
 const Login = () => {
-  const [signState, setSignState] = useState("Sign In")
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading , setLoading] = useState(false);
+  let [isSignInForm, setIsSignInForm] = useState(true);
+  let [errorMesssage, setErrorMessage] = useState(null);
+  let name = useRef(null); //{current:null}
+  let email = useRef(null);
+  let password = useRef(null);
 
+  let dispatch = useDispatch();
 
-  const user_auth = async (event)=>{
-    event.preventDefault();
-    setLoading(true);
-    if(signState==="Sign In"){
-      await login(email,password);
-    }else{
-      await signup(name,email,password);
-    }
-    setLoading(false);
-  }
+  const handleCheck = ()=>{
+    let message = isSignInForm
+      ? validateSignInValues(email.current.value, password.current.value)
+      : validateSignUpValues(
+          name.current.value,
+          email.current.value,
+          password.current.value
+        );
+    
+        setErrorMessage(message);
+        if (message) return;
+        if (isSignInForm) {
+          signInWithEmailAndPassword(
+            auth,
+            email.current.value,
+            password.current.value
+          )
+            .then((userCredential) => {
+              // Signed in
+              const user = userCredential.user;
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              setErrorMessage(errorCode + " " + errorMessage);
+            });
+        } else {
+          createUserWithEmailAndPassword(
+            auth,
+            email.current.value,
+            password.current.value
+          )
+            .then((userCredential) => {
+              // Signed up
+              const user = userCredential.user;
+              updateProfile(auth.currentUser, {
+                displayName: name.current.value,
+              })
+                .then(() => {
+                  const { uid, email, displayName } = auth.currentUser;
+                  dispatch(
+                    addUser({
+                      uid: uid,
+                      email: email,
+                      displayName: displayName,
+                    })
+                  );
+                })
+                .catch((error) => {
+                  setErrorMessage(error.message);
+                });
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              setErrorMessage(errorCode + " " + errorMessage);
+            });
+        }
+      };
+    
+      let change = () => {
+        setIsSignInForm(!isSignInForm);
+      };
 
   return (
     loading?<div className="login-spinner">
@@ -28,28 +84,54 @@ const Login = () => {
     </div>:
     <div className='login'>
       <img src={logo} alt="" className='login-logo' />
-      <div className="login-form">
-        <h1>{signState}</h1>
-        <form>
-          {signState==="Sign Up"?<input value={name} onChange={(e)=>{setName(e.target.value)}} type="text" placeholder='Your Name' />:<></>}
-          <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder='Your email' />
-          <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder='password' />
-          <button onClick={user_auth} type='submit'>{signState}</button>
-          <div className="form-help">
-            <div className="remember">
-              <input type="checkbox" />
-              <label htmlFor="">Remember Me</label>
-            </div>
-            <p> Need Help?</p>
+      <div className="body">
+      <div className="background-color">
+        <Header />
+        <div className="SignIn-page">
+          <div className="SignIn-container">
+            <form onSubmit={(a) => a.preventDefault()} className="SignIn-form">
+              <h1>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+              {!isSignInForm && (
+                <input
+                  ref={name}
+                  type="text"
+                  placeholder="Enter Your Name"
+                  required
+                />
+              )}
+              <input
+                ref={email}
+                type="email"
+                placeholder="Enter Your Email"
+                required
+              />
+              <input
+                ref={password}
+                type="password"
+                placeholder="Enter Your Password"
+                required
+              />
+              <p className="errorMessage">{errorMesssage}</p>
+              <button type="submit" onClick={handleCheck}>
+                {isSignInForm ? "Sign In" : "Sign Up"}
+              </button>
+              <p className="SignIn-or-SignUp">
+                {isSignInForm ? (
+                  <div>
+                    New to Netflix? <a onClick={change}>Sign Up</a> Now
+                  </div>
+                ) : (
+                  <div>
+                    Already registered? <a onClick={change}>Sign In</a> Now
+                  </div>
+                )}
+              </p>
+            </form>
           </div>
-        </form>
-        <div className="form-switch">
-          {signState==="Sign In"?<p>New To Netflix ? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p>
-          :<p>Already Have Account ? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now</span></p>}
         </div>
       </div>
+      </div>
     </div>
-  )
-}
-
+  );
+};
 export default Login
